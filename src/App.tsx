d0f86476dd4637aeafcf2ea899f0581358c96eee
Rafael Dalcolmo <dalcolmor@student.mville.edu>
2022-12-21 00:57:36 -0500

    Added Link to and ErrorPage

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,19 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Home from './Home/Home';
 import Command from './Command/Command';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ErrorPage from './Error';
 
 function App() {
   return (
-    <Fragment>
-          <NavMenu/>
-          <main className="container">
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/command" element={<Command/>}/>
-              </Routes>
-            </BrowserRouter>
-          </main>
-        </Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NavMenu/>}>
+            <Route index element={<Home/>}/>
+            <Route path="/command" element={<Command/>}/>
+            <Route path="*" element={<ErrorPage/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
