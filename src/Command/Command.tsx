18f98d65f74cd3eb01b2ec4d54b2bb3690f73295
Rafael Dalcolmo <dalcolmor@student.mville.edu>
2022-12-20 20:20:15 -0500

    Using react query

--- src/Command/Command.tsx
+++ src/Command/Command.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+import { useQuery } from 'react-query';
 
 
 function Command()
@@ -7,31 +8,35 @@ function Command()
     const [broadcasterId, setBroadcasterId] = useState('');
     const authorizationCode = new URLSearchParams((window.location.href).split("#")[1]).get('access_token') as string;
     const channel = new URLSearchParams(window.location.href).get('state');
-
-    useEffect(() =>
+    
+    if (!authorizationCode)
     {
-        if (!authorizationCode)
+        return null;
+    }
+    
+    async function fetchUser()
+    {
+        const response = await fetch(`https://api.twitch.tv/helix/users?login=${channel}`,
         {
-            return;
-        }
-        
-        async function GetBroadcastUserId()
-        { 
-            const result = await fetch('https://api.twitch.tv/helix/users?login=' + channel,
-                        {
-                            headers:
-                            {
-                                'Client-ID': '4qn897qcu5idrecjs143gi62s25ybg',
-                                'Authorization': 'Bearer ' + authorizationCode
-                            }
-                        });
-            const data = await result.json();
-            console.log(data);
+            headers:
+            {
+                'Client-ID': '4qn897qcu5idrecjs143gi62s25ybg',
+                'Authorization': 'Bearer ' + authorizationCode
+            }
+        });
+        return response.json();
+    }
+    
+    const { data, status } = useQuery(['accessToken', authorizationCode], fetchUser);
+    
+    useEffect(() => {
+        if (data)
+        {
+            console.log('Twitch login', {status, data});
             setBroadcasterId(data['data'][0].id);
             setIsVisible(true);
         }
-        GetBroadcastUserId();
-    }, []);
+    }, [data]);
    
     return (
         <div style={{display: isVisible ? "block" : "none"}}>
